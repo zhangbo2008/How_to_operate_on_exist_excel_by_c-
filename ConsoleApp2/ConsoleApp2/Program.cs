1bed3522cac9cca6a3225cd3407e3a3de7e25d4c
张博 <35842504+zhangbo2008@users.noreply.github.com>
2021-05-06 11:00:18 +0800

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+
+using Excel = Microsoft.Office.Interop.Excel;
+namespace Microsoft.Office.Interop
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Excel._Application objExcel;
+
+            objExcel = (Excel.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application");
+
+
+         //   objExcel.Workbooks.Open("c:/1.xlsx");
+            objExcel.Workbooks.Close();
+            objExcel.Workbooks.Open("c://1.xlsx");
+
+
+
+            System.Console.Write("dsafdsfsad");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
